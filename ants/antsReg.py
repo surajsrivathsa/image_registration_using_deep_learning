6887f745022ef118cbc2cf05f1e40c71ec598ce8
jacksparrow777 <suraj110693@outlook.com>
2022-03-02 07:43:01 +0530

    Update antsReg.py

--- ants/antsReg.py
+++ ants/antsReg.py
@@ -15,7 +15,7 @@ for fixed, moving in zip(file_names_f, file_names_m):
      print(os.path.basename(moving))
      t1_fixed = ants.image_read(fixed)
      t2_moving = ants.image_read(moving)
-     mytx = ants.registration(fixed=t1_fixed, moving=t2_moving, type_of_transform='SyN')
+     mytx = ants.registration(fixed=t1_fixed, moving=t2_moving, type_of_transform='Affine')
      warped_moving = mytx['warpedmovout']
      ants_output = '<result path>'+ os.path.basename(fixed)[:-7] + "_F_" + os.path.basename(moving)[:-7] + "_M.nii.gz"
      ants.image_write(warped_moving, ants_output, ri=False)
